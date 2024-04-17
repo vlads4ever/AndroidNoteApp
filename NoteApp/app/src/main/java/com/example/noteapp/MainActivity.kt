6563fc3e689aacdd54384797498b7b6edd9e81a4
vladimir_skrypnikov <sevgrad@list.ru>
2024-04-17 20:15:01 +0300

    Changes at MainActivity

--- NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.noteapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.noteapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
